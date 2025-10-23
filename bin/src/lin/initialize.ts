c9e584e24ff707ccd977a0d286086c8b5c50ec96
Hugo Chilemme <hugo.chilemme@gmail.com>
2025-10-23 18:58:35 +0200

    refactor initialize function for improved readability

--- bin/src/lin/initialize.ts
+++ bin/src/lin/initialize.ts
@@ -3,10 +3,11 @@ import initListenersPort from './utils/network/initListenersPort.js';
 import initUserInterface from './utils/auth/initUserInterface.js';
 
 async function initialize(): Promise<void> {
+
 	await createCacheDir();
 	await initListenersPort();
-
 	await initUserInterface();
+
 }
 
 void initialize();
